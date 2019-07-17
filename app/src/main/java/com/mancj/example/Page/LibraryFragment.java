c12d163876132872ba4a21667949da1ae94b74dd
Bian Suma <biansuma.163040084@mail.unpas.ac.id>
2019-07-17 16:01:27 +0700

    This file has been uploaded

--- app/src/main/java/com/mancj/example/Page/LibraryFragment.java
+++ app/src/main/java/com/mancj/example/Page/LibraryFragment.java
@@ -42,7 +42,7 @@ public class LibraryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_updates, container, false);
+        return inflater.inflate(R.layout.fragment_library, container, false);
     }
 
     @Override
